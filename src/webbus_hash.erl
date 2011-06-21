ca9de8bea78cf2b97b2b65463cf6bc265d3d9211
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-21 14:37:43 -0600

    Use SHA1 HMAC for hashing tripcodes

--- src/webbus_hash.erl
+++ src/webbus_hash.erl
@@ -1,9 +1,9 @@
 % Calculate hashes for client IDs from a given tripcode
 -module(webbus_hash).
 -export([data/1]).
--define(SERVER_KEY, <<"lol imma insecure key">>).
+-define(SERVER_KEY, <<"lol ama insecure key">>).
 
 data(Data) ->
-    <<IntHash:160/integer>> = crypto:sha([?SERVER_KEY, Data]),
+    <<IntHash:160/integer>> = crypto:sha_mac(?SERVER_KEY, Data),
     [StringHash] = io_lib:format("~40.16.0b", [IntHash]),
     list_to_binary(StringHash).
