1968de73be082d0259dce920688a57f0c6306cab
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-27 12:52:30 -0600

    Factor apart webbus_connection_handler and webbus_client_handler

--- src/webbus_connection_handler.erl
+++ src/webbus_connection_handler.erl
@@ -2,34 +2,19 @@
 -module(webbus_connection_handler).
 -behaviour(gen_event).
 -export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2, code_change/3]).
--include_lib("../deps/socketio/include/socketio.hrl").
--define(HASH_SIZE, 160).
 
-%% gen_event
 init([]) ->
     {ok, undefined}.
 
 handle_event({client, Pid}, State) ->
     io:format("Connected: ~p~n", [Pid]),
     EventMgr = socketio_client:event_manager(Pid),
-    ok = gen_event:add_handler(EventMgr, ?MODULE,[]),
+    Request =  socketio_client:request(Pid),
+    
+    ok = gen_event:add_handler(EventMgr, webbus_client_handler, [Request]),
     {ok, State};
 handle_event({disconnect, Pid}, State) ->
     io:format("Disconnected: ~p~n",[Pid]),
-    {ok, State};
-handle_event({message, Client, #msg{content = [{Command, Params}]}}, State) ->
-    handle_command(Command, Client, Params, State),
-    {ok, State};
-handle_event({message, Client, #msg{content = Content}}, State) ->
-    Response = [{<<"error">>, [
-        {<<"code">>, 400},
-        {<<"text">>, <<"Bad request">>},
-        {<<"message">>, Content}
-    ]}],
-    socketio_client:send(Client, #msg{json = true, content = Response}),
-    {ok, State};
-handle_event(_Event, State) ->
-    %% FIXME: Uhh, log unknown event or something?
     {ok, State}.
     
 handle_call(_, State) ->
@@ -43,30 +28,3 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-%%    
-
-handle_command(<<"register">>, Client, Params, State) ->
-    register_client(Client, Params),
-    {ok, State};
-handle_command(Command, Client, Params, State) ->
-    Response = [{<<"error">>, [
-        {<<"code">>, 404},
-        {<<"text">>, <<"Unknown command">>},
-        {<<"command">>, Command}, 
-        {<<"params">>, Params}
-    ]}],
-    socketio_client:send(Client, #msg{json = true, content = Response}),
-    {ok, State}.
-    
-register_client(Client, Params) ->
-    Tripcode = case proplists:get_value(<<"tripcode">>, Params) of
-        undefined ->
-            crypto:rand_bytes(?HASH_SIZE); % Generate a random tripcode
-        Bin when is_binary(Bin) ->
-            Bin
-    end,
-    
-    % FIXME: At some point snag the cleint IP out of here
-    % Request = socketio_client:request(Client),
-
-    io:format("Registering with tripcode: ~p~n", [Tripcode]).
